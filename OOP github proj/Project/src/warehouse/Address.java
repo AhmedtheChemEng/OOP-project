88aae782cca4b403ebdfef2672f2e54d202727d1
FTIYI90 <ftiyi.90@gmail.com>
2025-11-17 14:35:33 +0300

    done. Address.java
    DONE

--- OOP github proj/Project/src/warehouse/Address.java
+++ OOP github proj/Project/src/warehouse/Address.java
@@ -1,5 +1,40 @@
 package warehouse;
 
-public class Address {
+public class Adress {
+    private String street, city, country;
 
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Adress(String street, String city, String country) {
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+
+
+    public String toSingleLine() {
+        return street + " , " + city + " , " + country;
+    }
 }
