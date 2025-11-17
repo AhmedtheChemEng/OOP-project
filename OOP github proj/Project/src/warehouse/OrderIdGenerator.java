941bb267703bbba9abfdbfd58ae9087846666769
FTIYI90 <ftiyi.90@gmail.com>
2025-11-17 14:55:29 +0300

    DONE OrderIdGenerator.java
    FINISH!

--- OOP github proj/Project/src/warehouse/OrderIdGenerator.java
+++ OOP github proj/Project/src/warehouse/OrderIdGenerator.java
@@ -1,5 +1,9 @@
 package warehouse;
-
 public class OrderIdGenerator {
+    private static int counter=0;
+    public static String nextId(){
+        counter++;
 
+        return String.format("ORD-%04D",counter);
+    }
 }
