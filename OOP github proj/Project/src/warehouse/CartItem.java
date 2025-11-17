c1f35eadfd9a4325bfa95c11647731020773df9d
FTIYI90 <ftiyi.90@gmail.com>
2025-11-17 14:39:10 +0300

    done CartItem.java
    DONE!

--- OOP github proj/Project/src/warehouse/CartItem.java
+++ OOP github proj/Project/src/warehouse/CartItem.java
@@ -1,5 +1,44 @@
 package warehouse;
 
 public class CartItem {
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double lineSubTotal(){
+        return product.getPrice()*getQuantity();
+
+    }
+    public double lineWeight(){
+        return product.getWeightKg()*getQuantity();
+
+    }
+
+    
+    public String info() {
+        return "CartItem{" +
+                "product=" + product +
+                ", quantity=" + quantity +
+                '}';
+    }
 
 }
