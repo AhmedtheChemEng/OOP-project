7f05ad902f22204b9c504035f308f0d1a143dc74
FTIYI90 <ftiyi.90@gmail.com>
2025-11-17 14:53:12 +0300

    DONE ! OrderItem.java
    SI

--- OOP github proj/Project/src/warehouse/OrderItem.java
+++ OOP github proj/Project/src/warehouse/OrderItem.java
@@ -1,5 +1,42 @@
 package warehouse;
 
 public class OrderItem {
+    private Product product;
+    private int quantity;
+    private double unitPriceQar;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitPriceQar() {
+        return unitPriceQar;
+    }
+
+    public void setUnitPriceQar(double unitPriceQar) {
+        this.unitPriceQar = unitPriceQar;
+    }
+
+    public OrderItem(Product product, int quantity, double unitPriceQar) {
+        this.product = product;
+        this.quantity = quantity;
+        this.unitPriceQar = unitPriceQar;
+    }
+    public double lineTotal(){
+        return getUnitPriceQar()*getQuantity();
+
+    }
 
 }
