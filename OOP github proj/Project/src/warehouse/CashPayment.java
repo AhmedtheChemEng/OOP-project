63dd32f85d26248a76f1a10211964a1123ed086a
FTIYI90 <ftiyi.90@gmail.com>
2025-11-17 14:44:02 +0300

    DONE CashPayment.java
    DONEE!

--- OOP github proj/Project/src/warehouse/CashPayment.java
+++ OOP github proj/Project/src/warehouse/CashPayment.java
@@ -1,5 +1,48 @@
 package warehouse;
 
-public class CashPayment {
+public class CashPayment implements Payment {
+    private String currency;
+    private double amount;
 
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public CashPayment(String currency, double amount) {
+        this.currency = currency;
+        this.amount = amount;
+    }
+
+    @Override
+    public String currency() {
+        return getCurrency();
+
+    }
+
+    @Override
+    public double amount() {
+        return getAmount();
+
+    }
+
+    @Override
+    public String summary() {
+        return "CashPayment{" +
+                "currency='" + currency + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
+
