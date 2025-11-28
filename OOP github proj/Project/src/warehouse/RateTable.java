aac148876b60a042e6f27ce898db70a4d825ef9c
3lyanJ <125390859+3lyanJ@users.noreply.github.com>
2025-11-28 20:52:34 +0300

    Update RateTable.java

--- OOP github proj/Project/src/warehouse/RateTable.java
+++ OOP github proj/Project/src/warehouse/RateTable.java
@@ -1,5 +1,19 @@
 package warehouse;
 
 public class RateTable {
+	
+	double[] maxWeights = {2,5,10};
+	double[] feesQar = {10,20,35,50};
 
+	public double shippingFeeFor(double totalWeightKg) {
+		
+		for (int i=0;i<(maxWeights.length);i++) {
+			if (totalWeightKg<maxWeights[i]) {
+				return feesQar[i];
+			}
+		}
+		return feesQar[feesQar.length-1];
+	}
+	
+	
 }
