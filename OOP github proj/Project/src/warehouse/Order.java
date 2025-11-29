17c815d3b4afd2097cc5afbc9fd93193c5efa4fa
FTIYI90 <ftiyi.90@gmail.com>
2025-11-29 22:54:42 +0300

    Update Order.java

--- OOP github proj/Project/src/warehouse/Order.java
+++ OOP github proj/Project/src/warehouse/Order.java
@@ -1,102 +1,72 @@
-package warehouse;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Order {
+import java.util.ArrayList;
+import java.io.Serializable;
+
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String id;
     private Customer customer;
     private LocalDate date;
     private ArrayList<OrderItem> items;
-    private double subtotal, discountAmount, shippingFee, total;
+    private double subtotal;
+    private double discountAmount;
+    private double shippingFee;
+    private double total;
     private Discount appliedDiscount;
+    private Payment payment;
 
-    public Order(String id, Customer customer, ArrayList<OrderItem> items, double subtotal, double shippingFee, double total, Discount appliedDiscount) {
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public Order(String id, Customer customer, LocalDate date, ArrayList<OrderItem> items, double subtotal, double discountAmount, double shippingFee, double total, Discount appliedDiscount, Payment payment) {
         this.id = id;
         this.customer = customer;
-        this.date = LocalDate.now();
+        this.date = date;
         this.items = items;
         this.subtotal = subtotal;
         this.discountAmount = discountAmount;
         this.shippingFee = shippingFee;
         this.total = total;
         this.appliedDiscount = appliedDiscount;
+        this.payment = payment;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public Customer getCustomer() {
         return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
     }
 
     public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
     public ArrayList<OrderItem> getItems() {
         return items;
-    }
-
-    public void setItems(ArrayList<OrderItem> items) {
-        this.items = items;
     }
 
     public double getSubtotal() {
         return subtotal;
     }
 
-    public void setSubtotal(double subtotal) {
-        this.subtotal = subtotal;
-    }
-
     public double getDiscountAmount() {
         return discountAmount;
-    }
-
-    public void setDiscountAmount(double discountAmount) {
-        this.discountAmount = discountAmount;
     }
 
     public double getShippingFee() {
         return shippingFee;
     }
 
-    public void setShippingFee(double shippingFee) {
-        this.shippingFee = shippingFee;
-    }
-
     public double getTotal() {
         return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
     }
 
     public Discount getAppliedDiscount() {
         return appliedDiscount;
     }
-
-    public void setAppliedDiscount(Discount appliedDiscount) {
-        this.appliedDiscount = appliedDiscount;
-    }
-    
-    public String toString() {
-    	return String.format("%8s| %8s| QAR %.2f", id,customer.getName(),total);
-    }
-    
 }
