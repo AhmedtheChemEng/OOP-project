d1c8998e2568de37b9977b868ef1dbd13f43ad01
Ahmed <yacersebouai@gmail.com>
2025-11-29 19:08:01 +0300

    ProductListView Class

--- OOP github proj/Project/src/warehouse/ProductListView.java
+++ OOP github proj/Project/src/warehouse/ProductListView.java
@@ -6,6 +6,21 @@ public class ProductListView {
 	
 	
 	public static void printCategorized(ArrayList<Product> Product) {
+		for(int i=0;i<Product.size();i++) {
+			Product P=Product.get(i);
+			if(P instanceof ElectronicProduct) {
+				System.out.println(P);
+			}
+			
+		}
+		for(int i=0;i<Product.size();i++) {
+			Product P=Product.get(i);
+			if(P instanceof BookProduct) {
+				System.out.println(P);}}
 		
+		for(int i=0;i<Product.size();i++) {
+			Product P=Product.get(i);
+			if(P instanceof GroceryProduct) {
+				System.out.println(P);}}
 	}
 }
