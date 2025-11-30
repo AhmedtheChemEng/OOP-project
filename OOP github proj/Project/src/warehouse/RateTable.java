700f801b0910e6360b4a9d0005bfcc8732356d4b
3lyanJ <125390859+3lyanJ@users.noreply.github.com>
2025-11-30 04:30:07 +0300

    RateTable BugFix

--- OOP github proj/Project/src/warehouse/RateTable.java
+++ OOP github proj/Project/src/warehouse/RateTable.java
@@ -17,37 +17,6 @@ public class RateTable implements Serializable{
 	double[] maxWeights = {2,5,10};
 	double[] feesQar = {10,20,35,50};
 
-	
-	/**
-	 * Returns shipping fees from the total weight.
-	 *
-	 * <p>Returns one of the fee prices based on the totalWeight of the shipment.</p>
-	 *
-	 * @param totalWeightKg Total weight of shipment, to calculate fees, in double.
-	 * @return Shipping fees in double.
-	 */
-	public double shippingFeeFor(double totalWeightKg) {
-		
-		for (int i=0;i<(maxWeights.length);i++) {
-			if (totalWeightKg<maxWeights[i]) {
-				return feesQar[i];
-			}
-		}
-		return feesQar[feesQar.length-1];
-	}
-	
-	
-}
-
-package warehouse;
-import java.io.*;
-public class RateTable implements Serializable{
-
-	
-	
-	double[] maxWeights = {2,5,10};
-	double[] feesQar = {10,20,35,50};
-
 
 	public double shippingFeeFor(double totalWeightKg) {
 		
