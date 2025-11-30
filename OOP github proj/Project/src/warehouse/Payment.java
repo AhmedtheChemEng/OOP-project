4b89d5fe8394aee7bd5721f7d425432fb8a6e18f
FTIYI90 <ftiyi.90@gmail.com>
2025-11-30 04:00:22 +0300

    Update Payment.java

--- OOP github proj/Project/src/warehouse/Payment.java
+++ OOP github proj/Project/src/warehouse/Payment.java
@@ -1,3 +1,14 @@
+/**
+ * Defines the structure for different payment types in the warehouse system.
+ * Each payment must specify its currency, amount, and a summary description.
+ *
+ * Implementations include classes like CashPayment and CardPayment.
+ *
+ * @author Fahad Ali
+ * @version 3.0
+ * @since 2025-10-20
+ */
+
 package warehouse;
 import java.io.Serializable;
 
