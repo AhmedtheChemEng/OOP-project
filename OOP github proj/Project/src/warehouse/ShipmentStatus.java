08412ae11e62ad9a1c8538cc4051e14b28f1b629
FTIYI90 <ftiyi.90@gmail.com>
2025-11-30 04:07:06 +0300

    Update ShipmentStatus.java

--- OOP github proj/Project/src/warehouse/ShipmentStatus.java
+++ OOP github proj/Project/src/warehouse/ShipmentStatus.java
@@ -1,3 +1,16 @@
+/**
+ * Enumeration representing the various stages of a shipment process
+ * in the warehouse system. Used to track the delivery progress
+ * of customer orders.
+ *
+ * Possible statuses include:
+ * CREATED, PACKED, IN_TRANSIT, OUT_FOR_DELIVERY, and DELIVERED.
+ *
+ * @author Ahmed Seboui
+ * @version 2.0
+ * @since 2025-10-20
+ */
+
 package warehouse;
 
 public enum ShipmentStatus {
