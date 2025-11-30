633aaf9a23d6c4af113cdc8fced82f5ccea1b7fd
FTIYI90 <ftiyi.90@gmail.com>
2025-11-30 04:04:11 +0300

    Update RateTable.java

--- OOP github proj/Project/src/warehouse/RateTable.java
+++ OOP github proj/Project/src/warehouse/RateTable.java
@@ -1,3 +1,13 @@
+/**
+ * Represents the shipping rate table used in the warehouse system.
+ * Calculates shipping fees based on the total shipment weight.
+ * The rate table defines weight thresholds and corresponding QAR fees.
+ *
+ * @author Ali Al-Marri
+ * @version 3.0
+ * @since 2025-10-20
+ */
+
 package warehouse;
 import java.io.*;
 public class RateTable implements Serializable{
@@ -16,6 +26,29 @@ public class RateTable implements Serializable{
 	 * @param totalWeightKg Total weight of shipment, to calculate fees, in double.
 	 * @return Shipping fees in double.
 	 */
+	public double shippingFeeFor(double totalWeightKg) {
+		
+		for (int i=0;i<(maxWeights.length);i++) {
+			if (totalWeightKg<maxWeights[i]) {
+				return feesQar[i];
+			}
+		}
+		return feesQar[feesQar.length-1];
+	}
+	
+	
+}
+
+package warehouse;
+import java.io.*;
+public class RateTable implements Serializable{
+
+	
+	
+	double[] maxWeights = {2,5,10};
+	double[] feesQar = {10,20,35,50};
+
+
 	public double shippingFeeFor(double totalWeightKg) {
 		
 		for (int i=0;i<(maxWeights.length);i++) {
